e4c29e68a8eae8077c8ca695f48794c59ebed14f
KevinJordil <kevin.jordil@heig-vd.ch>
2022-02-06 17:46:53 +0100

    Remove Rbdimmer library

--- wemos/wemos.ino
+++ wemos/wemos.ino
@@ -1,15 +1,13 @@
-
 #include "Arduino.h"
 
 #include <NTPClient.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
-#include <RBDmcuESP8266.h>
 #define PORT 80
 #define LAMP_STATES 100
 
-const char* ssid = "Wifi Kevin 2.5";
-const char* password = "YverdonBeach";
+const char* ssid = "";
+const char* password = "";
 
 //Parameters
 const int switchButton  = D5; //11;
@@ -17,10 +15,8 @@ const int zeroCrossPin  = D6; //12;
 const int acdPin  = D7; //13;
 bool lampOn = true;
 int lastRead = -1;
-
-
-//Objects
-dimmerLamp acd(acdPin, zeroCrossPin);
+int delay_time;
+int lamp_intensity = 0;
 
 
 ESP8266WebServer server(80);
@@ -28,29 +24,48 @@ WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", 7200);
 int startTimestamp = -1, durationTimestamp = -1, epochStates = -1, lastPower = 0;
 
+void ICACHE_RAM_ATTR interrupt() {
+  if (lamp_intensity != 0) {
+    delay_time = (100 - (lamp_intensity * 0.5)) * 100;
+    delay_time = delay_time < 250 ? 250 : delay_time;
+    
+    delayMicroseconds(delay_time);
+    
+    digitalWrite(acdPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(acdPin, LOW);
+  }
+}
 
 void setup() {
 
-  // Connect to wifi
+
   Serial.begin(115200);
   Serial.println("Begin...");
-  acd.begin(NORMAL_MODE, ON);
+  
+  pinMode(LED_BUILTIN, OUTPUT); // Led on Wemos
+  digitalWrite(LED_BUILTIN, LOW); // led On
+  
   pinMode(switchButton, INPUT_PULLUP);
   digitalWrite(switchButton, HIGH);
-  acd.setPower(10);
+
+  pinMode(acdPin, OUTPUT);
+  pinMode(zeroCrossPin, INPUT_PULLUP);
+  
+  attachInterrupt(digitalPinToInterrupt(zeroCrossPin), interrupt, RISING);
+  
+
   Serial.println("Connect to wifi...");
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
-    acd.setPower(1);
-    delay(250);
-    acd.setPower(10);
-    delay(250);
+    Serial.println("Try to connect...");
+    delayMicroseconds(16383); // Max miscroseconds time
   }
   Serial.println("Connected!");
   Serial.println("Start web server...");
-  acd.setPower(10);
+  
   server.on("/", HTTP_POST, []() {
 
     Serial.println("Get request");
@@ -64,19 +79,16 @@ void setup() {
 
 
     server.send(200, "text/plain", "Timestamp receive");
-    acd.setPower(20);
-    delay(1000);
-    acd.setPower(lastPower);
   });
 
   server.begin();
   timeClient.begin();
   timeClient.update();
-  delay(500);
-  acd.setPower(20);
-  delay(2000);
-  acd.setPower(1);
+
+  
+  lamp_intensity = 0;
   Serial.println("Listening...");
+  digitalWrite(LED_BUILTIN, HIGH); 
 }
 
 void loop() {
@@ -88,7 +100,7 @@ void loop() {
     lampOn = !lampOn;
   }
 
-  if (startTimestamp != -1 && timeClient.getEpochTime() >= startTimestamp && lastPower == 0){
+  if (startTimestamp != -1 && timeClient.getEpochTime() >= startTimestamp && lastPower == 0) {
     Serial.println("Ready to start intensity");
     lampOn = true;
   }
@@ -96,28 +108,26 @@ void loop() {
   if (lampOn) {
     if (startTimestamp != -1 && timeClient.getEpochTime() >= startTimestamp && startTimestamp + durationTimestamp > timeClient.getEpochTime()) {//Allumer lampe avec intensité
 
-      
+
       int intensity = ((timeClient.getEpochTime() - startTimestamp) / epochStates) + 1;
       if (lastPower != intensity) {
         Serial.print("Set intensity : ");
         Serial.println(intensity);
-        acd.setPower(intensity);
+        lamp_intensity = intensity;
         lastPower = intensity;
       }
 
     } else if (lastPower != 100) {
       startTimestamp = -1;
       lastPower = 100;
-      acd.setPower(100);
+      lamp_intensity = 100;
       Serial.println("ON");
     }
   } else if (lastPower != 0) {
-      startTimestamp = -1;
-      lastPower = 0;
+    startTimestamp = -1;
+    lastPower = 0;
   } else {
-      acd.setPower(1);
-      Serial.println("OFF");
+    lamp_intensity = 0;
   }
 
-  //delay(1000);
 }
